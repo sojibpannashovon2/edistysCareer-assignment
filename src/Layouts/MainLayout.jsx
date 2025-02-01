6ae1aa3d3e76849378077a364832339e4f515a4a
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2025-02-01 23:50:40 +0600

    installation complete of tailwind css with daisy ui, react router

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Navber from "../components/Shared/Navber";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navber />
+    </div>
+  );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "./Layouts/MainLayout.jsx";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<App />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
